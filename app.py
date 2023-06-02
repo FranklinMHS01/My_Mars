b30aeb740f4b4bcb9f672f51f8cb7ed0045658b9
FranklinMHS01 <franklinchang0129@gmail.com>
2023-06-02 17:16:16 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://franklinchang08:123456788@felix08.jreclb8.mongodb.net/')
 
-db = client.dbmars
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
+client = MongoClient('MONGODB_URI')
+db = client[DB_NAME]
+
 app = Flask(__name__)
 
 @app.route('/')
